c65920df22356038b0a30dbb27368ffb063a830f
fabrice137 <fabruz3@gmail.com>
2024-08-14 11:27:25 +0200

    add public web

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const app = express();
 
-
+const port = process.env.PORT || 5000;
 
 // app.use('/'+ express.static(path.join(__dirname, '/react-app/build')));
 
 // app.use('/website'+ express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/web')));
 
 
 
@@ -24,6 +24,6 @@ app.get('/api', (req, res) =>{
 
 
 
-app.listen(3000, () =>{
-    console.log("App listening on port 3000");
+app.listen(port, () =>{
+    console.log(`App listening on port ${port}`);
 })
